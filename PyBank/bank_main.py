f2b4461492f1f8b4933e9b497d07933fca50af89
AstarothGundam92 <emste.mlis@gmail.com>
2018-09-26 22:28:22 -0700

    almost done bank_main.py and halfway through poll_main.py

--- PyBank/bank_main.py
+++ PyBank/bank_main.py
@@ -1,32 +1,62 @@
+# importing packages and data csv
 import os
 import csv
+import sys
 bank_csv = os.path.join("./budget_data.csv")
 
-months = 0
+# defining lists and variables
+months = []
+earnings = []
+difList = []
+bank_dict = {}
 netTotal = 0 
 averageChange = 0
+greatestProfit = 0
+greatestLoss = 0
 
-
-print(bank_csv)
+# opening and defining csvreader to manipulate further
 with open(bank_csv,newline="") as bankcsvfile:
     csvreader = csv.reader(bankcsvfile,delimiter=",")
-    # print(csvreader)
-    # prints header to remove it from subsequent for loop
+    # printing header to remove it from subsequent for loop
     csv_header = next(csvreader)
-    # print(csv_header)
-    # for loop
+
+# using for loops to create lists and determine the total 
+# earned and average change
     for row in csvreader:
-        months = months + 1
         netTotal += int(row[1])
-        
-    
-# Printing Output
-print()
-print("Financial Analysis")
-print("-------------------------")
-print(f"Total Months: {months}")
-print(f"Total: ${netTotal}")
-print(f"Average Change: ${averageChange}")
-# print(f"Greatest Increase in Profits: {  } (${  })")
-# print(f"Greatest Decrease in Profits: {  } (${  })")
-# output = open("bank_data.txt","w")
+        months.append(row[0])
+        earnings.append(row[1])
+    for i in range(1, int(len(months))):
+        difList.append(int(earnings[i]) - int(earnings[i-1]))
+        averageChange = sum(difList) / int(len(months) - 1)
+# using max() and min() to find the greatest increase in 
+# profits and greatest decrease in losses, as well as 
+# their respective months; + 1 to ensure that the first month 
+# is not counted so the lists align properly
+    greatestProfit = max(difList)
+    greatestProfitmonth = months[difList.index(max(difList)) + 1]
+    greatestLoss = min(difList)
+    greatestLossmonth = months[difList.index(min(difList)) + 1]
+
+# printing data summary
+    def dataSummary():
+        print()
+        print("Financial Analysis")
+        print("-------------------------")
+        print(f"Total Months: {int(len(months))}")
+        print(f"Total: ${netTotal}")
+        print(f"Average Change: ${round(averageChange, 2)}")
+        print(f"Greatest Increase in Profits: {greatestProfitmonth} (${greatestProfit})")
+        print(f"Greatest Decrease in Profits: {greatestLossmonth} (${greatestLoss})")
+        return
+
+    dataSummary()
+
+# putting data summary as a .txt file output
+#sys.stdout = open('bank_data.txt','w'
+
+
+# # # trying to figure out how to print print statements
+# f = open("bank_data.txt","x")
+# f.write()
+# f.close()
--- PyPoll/poll_main.py
+++ PyPoll/poll_main.py
@@ -1,36 +1,47 @@
+# importing packages and data csv
 import os
 import csv
-
 poll_csv = os.path.join("./election_data.csv")
 
+# defining lists and variables
 voters = 0
-
+voterID = []
+candidate = []
+voteDict = {}
 winner = 0
 
-print(poll_csv)
+
+# opening and defining csvreader to manipulate further
 with open(poll_csv,newline="") as pollcsvfile:
     csvreader = csv.reader(pollcsvfile,delimiter=",")
-    # print(csvreader)
     # prints header to remove it from subsequent for loop
     csv_header = next(csvreader)
     # print(csv_header)
     # for loop
     for row in csvreader:
         voters = voters + 1
-        
+        voterID.append(row[0])
+        candidate.append(row[2])
+        voteDict = dict(zip(voterID, candidate))
+    #print(voteDict)
         
     
-# Printing Output
+    #winner = max()
+
+# printing data summary
 print()
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {voters}")
 print("-------------------------")
-# print(f"  {   }")
-# print(f"  {   }")
-# print(f"  {   }")
-# print(f"  {   }")
+# print(f"{   }: {round(   , 2)}% ({  }) ")
+# print(f"{   }: {round(   , 2)}% ({  }) ")
+# print(f"{   }: {round(   , 2)}% ({  }) ")
+# print(f"{   }: {round(   , 2)}% ({  }) ")
 print("-------------------------")
-# print(f"Winner: {  }")
+# print(f"Winner: {winner}")
+
+
+# putting data summary as a .txt file output
 # output = open("poll_data.txt","w")
 
